31a08771ecbf8e09e178b8c955c86e534fa92372
awojt74 <awojtowicz2@protonmail.com>
2025-06-06 08:52:06 +0200

    Update Test1.java

--- Test/src/test/java/Test1.java
+++ Test/src/test/java/Test1.java
@@ -3,7 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MainTest {
+public class Test1 {
     @Test
     public void testAdd() {
         assertEquals(5, Main.add(2, 3));
