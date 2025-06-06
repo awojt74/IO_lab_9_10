3d9adc988f3cadd0c7cc1fe0afbabd59e5d158b1
awojt74 <awojtowicz2@protonmail.com>
2025-06-06 10:47:47 +0200

    test_poprawa

--- Test/src/test/java/Test1.java
+++ Test/src/test/java/Test1.java
@@ -6,6 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Test1 {
     @Test
     public void testAdd() {
-        assertEquals(5, Main.add(2, 3))
+        assertEquals(5, Main.add(2, 3));
     }
 }
