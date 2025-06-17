ff289d1a6c69f45938ad92ceeb2465251c608dbd
awojt74 <awojtowicz2@protonmail.com>
2025-06-17 13:30:54 +0200

    Update Test1.java

--- Test/src/test/java/Test1.java
+++ Test/src/test/java/Test1.java
@@ -7,5 +7,22 @@ public class Test1 {
     @Test
     public void testAdd() {
         assertEquals(5, Main.add(2, 3));
+        assertEquals(0, Main.add(-1, 1));
     }
+
+    @Test
+    public void testDivide() {
+        assertEquals(5.0, Main.divide(10, 2));
+        assertThrows(ArithmeticException.class, () -> Main.divide(10, 0));
+    }
+
+    @Test
+    public void testCheckNumber() {
+        assertEquals("Negative", Main.checkNumber(-5));
+        assertEquals("Zero", Main.checkNumber(0));
+        assertEquals("Positive", Main.checkNumber(50));
+        assertEquals("Large", Main.checkNumber(101));
+    }
+
+
 }
